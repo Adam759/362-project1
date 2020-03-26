bc1a06d070c44c631b5438133a918dfc8d0c7763
Adam759 <adam759@comcast.net>
2020-03-25 23:40:21 -0500

    More alert migration

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 function alert_message(mes){
-  alert(mes);
   $(".alert_text").text(mes);
   $(".alert_container").css('display','block');
 }
@@ -9,19 +8,17 @@ $(document).ready(function(){
     if (/^[a-zA-Z ]+$/.test($("#namein").val())){
       if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test($("#emailin").val())){
         if ($("#interestsin").val() !== ""){
-          alert("Your info has been recorded. Check your email every Friday for new and exciting things! **This is not a real service, for now...**");
+          alert_message("Your info has been recorded. Check your email every Friday for new and exciting things! **This is not a real service, for now...**");
         } else {
-          alert("Please enter at least one interest! Everyone has at least one.");
+          alert_message("Please enter at least one interest! Everyone has at least one.");
           return false;
         }
       } else {
-        alert("Please enter a valid email");
         alert_message("Please enter a valid email");
         return false;
       }
     } else {
-      alert("Please enter a valid name");
-      alert($(".alert_container").get());
+      alert_message("Please enter a valid name");
       return false;
     }
   });
