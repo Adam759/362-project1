aff272b08631b3245d5036b9e1e59da1d379a449
Adam759 <adam759@comcast.net>
2020-03-25 22:41:31 -0500

    CSS Alert Box Start

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
   $("#info_form").submit(function(){
     if (/^[a-zA-Z ]+$/.test($("#namein").val())){
       if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test($("#emailin").val())){
@@ -18,5 +17,4 @@ $(document).ready(function(){
       return false;
     }
   });
-
 });
